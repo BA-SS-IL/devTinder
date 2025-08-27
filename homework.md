fe6f4111afe8955d7aa50ff30bcadc997ee2591e
Basil Varghese <basilvarghese@Basils-MacBook-Air.local>
2025-08-27 17:34:46 +0530

    exploring http methods

---
+++ homework.md
@@ -0,0 +1,28 @@
+day 1
+
+create a repository
+Initialize the repository
+node_modules, package.json , package-lock.json
+install express
+create a server
+listen  a server
+write request handlers for /tese, /hello
+Install nodemon and update scripts inside package.json
+what are dependencies
+what is the use of "-g" while npm install
+differents between caret and tilde(^ vs ~)
+
+day-2
+
+initialized git
+.gitignore
+create a remote repository github
+push all code remote to origin
+play with routes and route extension eg : /hello, / , hello/2, xyx
+order of routes is very important
+install postman app and make a workplace /collection >test api call
+write logic and test http methods on post man
+
+
+
+
--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 
+//this will match all the http methods api calls if write above all other methods (it will over ride)
+
 app.use('/',(req,res)=>{
-    res.send('hello from the dashboard')
+    res.send('hahahahah')
 })
 
-app.use('/hello',(req,res)=>{
-    res.send('hello hello heloo')
+//this will only handle get calls to /user
+app.get('/user',(req,res)=>{
+    res.send({firstName:"Basil",lastName:"Varghese"})
+});
+
+
+app.post('/user',(req,res)=>{
+    res.send('Data successfully saved to the data base')
+});
+
+app.delete('/user',(req,res)=>{
+    res.send('successfully deleted data')
+});
+
+
+app.put('/user',(req,res)=>{
+    res.send(' user data changed ')
 })
 
-app.use('/test',(req,res)=>{
-    res.send('hello from the test')
+app.patch('/user',(req,res)=>{
+    res.send('user data changed partially ')
 })
 
-app.listen(7777,()=>{
+app.listen(3000,()=>{
     console.log('server is successfully listning on port 3000')
 });
